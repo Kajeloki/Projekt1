eb0ecb8ffa27d853ee826bad824b8e38202c0d89
Kajeloki <101186819+Kajeloki@users.noreply.github.com>
2022-06-22 13:39:16 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,33 @@
+import java.util.Scanner;
+import java.lang.Math;
+class Main {
+  public static void main(String[] args) {
+    Scanner scanner = new Scanner(System.in);
+    System.out.println("Podaj liczbe x");
+    int liczba=0;
+    int najwieksza=0;
+    int prawienajwieksza=0;
+    int i=0;
+    liczba=scanner.nextInt();
+    while(liczba!=123)
+      {
+        if(liczba>najwieksza)
+        {
+          prawienajwieksza=najwieksza;
+          najwieksza=liczba;
+          
+        }
+         if(liczba>prawienajwieksza&&liczba<najwieksza)
+        {
+          prawienajwieksza=liczba;
+          
+          
+        }
+       
+        i++;
+        liczba=scanner.nextInt();
+      }
+     System.out.println("Najwieksza "+najwieksza);
+    System.out.println("Prawie najwieksza "+prawienajwieksza);
+  }
+}
